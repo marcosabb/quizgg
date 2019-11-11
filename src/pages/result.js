8a9297d7f402f57462a13f33c3b58bdde1e97f9b
Marcos Borges <marcos.abb52@gmail.com>
2019-11-11 20:06:15 -0300

    Add initial result page

---
+++ src/pages/result.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Seo from '../components/Seo'
+
+const Result = () => (
+  <div>
+    <Seo title='Eu acertei 5 perguntas, você consegue me passar?' />
+  </div>
+)
+
+export default Result
