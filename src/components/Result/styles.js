54690b63bc0c28fb49da08afbdc7fa6935b86681
Marcos Borges <marcos.abb52@gmail.com>
2019-11-13 21:05:33 -0300

    Fix question and result responsive

--- src/components/Result/styles.js
+++ src/components/Result/styles.js
@@ -11,6 +11,11 @@ export const Container = styled.div`
   border-radius: 6px;
   border: 1px solid ${rgba(color.blue, 0.04)};
   box-shadow: 0 20px 20px -20px ${rgba(color.blue, 0.2)};
+
+  ${media.lessThan('medium')`
+    padding-left: 10px;
+    padding-right: 10px;
+  `}
 `
 
 export const Statement = styled.p`
